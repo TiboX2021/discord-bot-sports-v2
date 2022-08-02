fac758b1e2a2ce1479f8c213fa3d19e96b8a6b4e
TiboX2021 <thibaut2saivre@gmail.com>
2022-08-02 17:34:07 +0200

    parser completed

--- parser.py
+++ parser.py
@@ -1,7 +1,38 @@
 """
-Parser : TODO
+Find the first keyword following a prefix
 """
 
 
-class Parser:
-    pass
+def parse_keyword(s: str, prefix: str) -> str | None:
+    """:returns the first string that follows a prefix, else None if none was found"""
+
+    # Searching for the prefix
+    index = s.find(prefix)
+
+    if index == -1:  # Prefix not found
+        return None
+
+    length = len(s)
+
+    # Scanning next word
+    # Skip through 's' until the next letter (skips symbols, spaces, ...)
+    index += 1
+    while not s[index].isalpha():
+        index += 1
+        if index >= length:  # Reached end of string
+            return None
+
+    # Find end of word
+    end_index = index + 1
+    while end_index < length - 1 and s[end_index].isalpha():
+        end_index += 1
+
+    return s[index:end_index]
+
+
+if __name__ == "__main__":
+    # Testing
+
+    test_str = "ex : 1)/ - sport I chose"
+
+    print(parse_keyword(s=test_str, prefix='1'))
